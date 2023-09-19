ad5b945a9e5b61833dd34a37c112d4d8dab5ed21
unknown <Akane-Asahi>
2023-09-19 21:11:56 +0600

    3rd commit

--- test_and_label.sh
+++ test_and_label.sh
@@ -3,17 +3,19 @@
 # Check if the "index.html" file exists
 if [ ! -f index.html ]; then
   echo "index.html not found. Test failed."
-  exit 1
-fi
-
-# Check if the first line of "index.html" contains "<!DOCTYPE html>"
-if head -n 1 index.html | grep -q "<!DOCTYPE html>"; then
-  echo "Test passed. Adding 'pass' label to the commit."
-  # Replace 'your_github_token' with your actual GitHub token
-  curl -X POST -H "Authorization: token MY_TOKEN" -d '{"labels":["pass"]}' https://api.github.com/repos/Akane-Asahi/check-labels/issues/$(git rev-parse --abbrev-ref HEAD | sed 's/[^0-9]*//g')/labels
 else
-  echo "Test failed. Adding 'flaky' label to the commit."
-  # Replace 'your_github_token' with your actual GitHub token
-  curl -X POST -H "Authorization: token MY_TOKEN" -d '{"labels":["flaky"]}' https://api.github.com/repos/Akane-Asahi/check-labels/issues/$(git rev-parse --abbrev-ref HEAD | sed 's/[^0-9]*//g')/labels
-  exit 1
+  # Check if the first line of "index.html" contains "<!DOCTYPE html>"
+  if head -n 1 index.html | grep -q "<!DOCTYPE html>"; then
+    echo "Test passed. Adding 'pass' label to the commit."
+    # Replace 'MY_TOKEN' with your actual GitHub token
+    # Replace 'my-username' with your GitHub username
+    # Replace 'my-repository' with your GitHub repository name
+    curl -X POST -H "Authorization: token MY_TOKEN" -d '{"labels":["pass"]}' "https://api.github.com/repos/Akane-Asahi/check-labels/issues/$(git rev-parse --abbrev-ref HEAD)/labels"
+  else
+    echo "Test failed. Adding 'flaky' label to the commit."
+    # Replace 'MY_TOKEN' with your actual GitHub token
+    # Replace 'my-username' with your GitHub username
+    # Replace 'my-repository' with your GitHub repository name
+    curl -X POST -H "Authorization: token MY_TOKEN" -d '{"labels":["flaky"]}' "https://api.github.com/repos/Akane-Asahi/check-labels/issues/$(git rev-parse --abbrev-ref HEAD)/labels"
+  fi
 fi
